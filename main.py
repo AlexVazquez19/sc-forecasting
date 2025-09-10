8d2e6f5967220b2c2f7c8df9a0826e1d745f9a5c
Alex Vazquez <86645097+AlexVazquez19@users.noreply.github.com>
2025-09-09 22:45:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv("train.csv")   # or a path like "data/train.csv" if it lives in a subfolder
+df = pd.read_csv("Data/train.csv")   # or a path like "data/train.csv" if it lives in a subfolder
 print(df.head())
